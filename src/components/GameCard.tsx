22d416c456082975dac91c4027eb7b453673b00e
jasmineka26 <149463857+jasmineka26@users.noreply.github.com>
2023-12-13 16:45:53 +0330

    Get optimized images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -9,6 +9,7 @@ import {
 import { Game } from "../hooks/useGames";
 import PlarformIconList from "./PlarformIconList";
 import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   game: Game;
@@ -16,10 +17,10 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card borderRadius={10} overflow={"hidden"}>
-      <Image src={game.background_image} />
-      <CardBody>
+      <Image src={getCroppedImageUrl(game.background_image)} />
+      <CardBody paddingX={2}>
         <Heading fontSize={"md"}>{game.name}</Heading>
-        <HStack justify={"space-between"}>
+        <HStack justifyContent="space-between">
           <PlarformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
